c5682c14349f52434a56342f21aa43c2373aa046
Eshwar Lal <151713880+EshwarLal21@users.noreply.github.com>
2026-01-13 16:34:50 +0500

    swap to variable without temp
    Swap with arthimatic and Xor operator

---
+++ Exercise3.java
@@ -0,0 +1,22 @@
+public class Exercise3 {
+
+    // Function to swap two numbers using arithmetic operations
+    public static int[] swapUsingArithmetic(int a, int b) {
+        // TODO: Implement this function
+        a = a + b;
+        b = a- b;
+        a = a - b;
+        
+        return new int[] {a,b};
+    }
+
+    // Function to swap two numbers using bitwise XOR
+    public static int[] swapUsingXOR(int a, int b) {
+        // TODO: Implement this function
+         a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        
+        return new int[] {a,b};
+    }
+}
