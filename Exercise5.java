2449608f56d330794576b89b2f97e040e5b5be80
Eshwar Lal <151713880+EshwarLal21@users.noreply.github.com>
2026-01-13 16:45:01 +0500

    multiplication of floating number

---
+++ Exercise5.java
@@ -0,0 +1,16 @@
+public class Exercise5 {
+
+    // Function to multiply two floating-point numbers
+    public static double multiplyFloatingPoints(double num1, double num2) {
+        
+        return num1 * num2;
+        // TODO: Implement this function
+    }
+
+    // Function to return the result of multiplication
+    public static double getMultiplicationResult(double num1, double num2) {
+        // TODO: Implement this function
+        
+        return multiplyFloatingPoints(num1,num2);
+    }
+}
